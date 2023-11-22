22d77298825643339a0be3a774dbb98aeb870c13
Manjeet <manjeetkamboj394@gmail.com>
2023-11-22 22:50:57 +0530

    made a websocket connection to the client

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());  // Parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on('connection', (socket) => {
+    console.log('A client connected');
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+});
+
+app.post('/', (req, res) => {
+    const message = req.body.char;
+    console.log(message);
+    
+    io.emit('recieve', {char : message});
+
+    res.status(200).json({ status: 'success'});
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
