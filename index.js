59573050376a231df2ff4e84d6bf61b0b021bf44
lavish <goellavish10@gmail.com>
2024-05-28 11:56:55 +0530

    api call to ml model

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+const axios = require("axios");
 
 app.use(cors());
 app.use(express.json()); // Parse JSON bodies
@@ -14,13 +19,13 @@ let history = {
   sentence: "",
   lastWord: "",
   currentWord: "",
-  suggestedWords: [],
+  suggestedWords: []
 };
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-  },
+    origin: "*"
+  }
 });
 
 io.on("connection", (socket) => {
@@ -35,25 +40,83 @@ app.get("/", (req, res) => {
   res.send("Server connected!");
 });
 
-app.post("/nodemcu", (req, res) => {
+app.post("/nodemcu", async (req, res) => {
   console.log("Request Received!");
   const char = req.body.message;
+  const mode = req.body.mode;
   console.log(char);
-  if (char == " ") {
-    history.lastWord = history.currentWord;
-    history.currentWord = "";
-    history.sentence += char;
-  } else {
-    history.currentWord += char;
-    history.sentence += char;
+  if (mode === "0") {
+    if (char == " ") {
+      console.log("REACHING SPACE");
+      history.lastWord = history.currentWord;
+      history.currentWord = "";
+      history.sentence += char;
+      // make api call to ML model
+      const response = await axios.post(
+        "https://next-word-prediction-43k6.onrender.com/predict",
+        {
+          sentence: history.sentence
+        }
+      );
+      history.suggestedWords = response.data[0];
+    } else {
+      history.currentWord += char;
+      history.sentence += char;
+    }
+    console.log("Request Received!");
+    io.emit("recieve", {
+      alphabet: char,
+      suggestions: history.suggestedWords,
+      sentence: history.sentence
+    });
+    console.log(history);
+    res.status(200).json({ status: "success" });
+  } else if (mode === "1") {
+    if (char === "!") {
+      history.sentence.slice(0, -1);
+      io.emit("recieve", {
+        alphabet: char,
+        suggestions: ["Manjeet", "Man", "Mannu"],
+        sentence: history.sentence
+      });
+    }
+
+    if (char === "a" || char === "b" || char === "c") {
+      history.sentence +=
+        history.suggestedWords[char === "a" ? 0 : char === "b" ? 1 : 2];
+      console.log("INSIDE MODE 1");
+      console.log(history.sentence);
+      history.lastWord = history.currentWord;
+      history.currentWord = "";
+      history.sentence += " ";
+      const response = await axios.post(
+        "https://next-word-prediction-43k6.onrender.com/predict",
+        {
+          sentence: history.sentence
+        }
+      );
+      history.suggestedWords = response.data[0];
+      io.emit("recieve", {
+        alphabet: char,
+        suggestions: history.suggestedWords,
+        sentence: history.sentence
+      });
+    }
+
+    if (char === "d") {
+      console.log("ALERT");
+      client.messages
+        .create({
+          body: "Hi this is an alert message, I need help!",
+          to: "+918847099350",
+          from: "+13203146245"
+        })
+        .then((message) => console.log(message.sid));
+      console.log("ALERT SENT");
+    }
+
+    res.status(200).json({ status: "success" });
   }
-  console.log("Request Received!");
-  io.emit("recieve", {
-    alphabet: char,
-    suggestions: ["Manjeet", "Man", "Mannu"],
-  });
-  console.log(history);
-  res.status(200).json({ status: "success" });
 });
 
 server.listen(3001, () => {
